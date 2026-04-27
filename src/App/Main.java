5faffbc68b43a2244c0850c812e269a7d703122b
Роман <klymenkoroman89@gmail.com>
2026-04-27 15:01:55 +0300

    Update app info

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,7 +1,7 @@
 package App;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My Git App.");
+        System.out.println("My Git App for users");
 
     }
 }

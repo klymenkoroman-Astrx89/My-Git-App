24ccb0ae8f1880ba00791cf3538dd164af020bae
Роман <klymenkoroman89@gmail.com>
2026-04-27 15:09:39 +0300

    Add app description

--- src/App/Main.java
+++ src/App/Main.java
@@ -2,6 +2,7 @@ package App;
 public class Main {
     public static void main(String[] args) {
         System.out.println("My Git App for users");
+        System.out.println("The informative application.");
 
     }
 }
